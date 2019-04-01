d642e400c8ff24452f16c3bbf6e6eae5401c71eb
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-01 15:05:16 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors && person == "Freddy Mercury"
-        attribute.each do |flavor|
+        value.each do |flavor|
           if flavor == "strawberry"
             attribute.remove[flavor]
           end
